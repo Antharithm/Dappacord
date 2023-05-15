ab2aa7c11bea94706bfad594bfde24832e63d1cb
Anthony <antharhythm@Anthonys-MacBook-Pro.local>
2023-05-15 11:36:15 -0400

    wrote test for erc721 name

--- test/Dappacord.test.js
+++ test/Dappacord.test.js
@@ -1,9 +1,24 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
-describe("Dappacord", function () {
+describe("Dappcord", function () {
+  describe("Deployment", function () {
+    it("Should set the name", async () => {
+      const Dappcord = await ethers.getContractFactory("Dappcord");
+      dappcord = await Dappcord.deploy("Dappcord", "DC");
 
-})
+      // Fetch name
+      let result = await dappcord.name();
+      // Check name
+      expect(result).to.equal("Dappcord");
+
+      // Fetch symbol
+      result = await dappcord.symbol();
+      // Check symbol
+      expect(result).to.equal("DC");
+    });
+  });
+});
