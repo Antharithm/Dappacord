d39b8a3585a896147a2c0d0fc2babd3f04605186
Anthony <antharhythm@Anthonys-MacBook-Pro.local>
2023-05-15 16:36:41 -0400

    added createChannel function

--- test/Dappacord.test.js
+++ test/Dappacord.test.js
@@ -19,6 +19,12 @@ describe("Dappcord", function () {
     // Deploy the smart contract
     const Dappcord = await ethers.getContractFactory("Dappcord");
     dappcord = await Dappcord.deploy(NAME, SYMBOL);
+
+    // Creat a channel
+    const transaction = await dappcord
+      .connect(deployer)
+      .createChannel("general", tokens(1));
+    await transaction.wait();
   });
 
   describe("Deployment", function () {
@@ -39,6 +45,20 @@ describe("Dappcord", function () {
     it("Should set the owner", async () => {
       const result = await dappcord.owner();
       expect(result).to.equal(deployer.address);
+    });
+
+    describe("Creating Channels", () => {
+      it("Should return the total amount of channels", async () => {
+        const result = await dappcord.totalChannels();
+        expect(result).to.be.equal(1);
+      });
+
+      it("Should return the channel attributes", async () => {
+        const channel = await dappcord.getChannel(1);
+        expect(channel.id).to.be.equal(1);
+        expect(channel.name).to.be.equal("general");
+        expect(channel.cost).to.be.equal(tokens(1));
+      });
     });
   });
 });
