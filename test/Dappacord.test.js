7849d4a1b05b64ac1eb255cf45a063d41d3c8fd8
Anthony <antharhythm@Anthonys-MacBook-Pro.local>
2023-05-15 16:15:17 -0400

    formatted test and added owner

--- test/Dappacord.test.js
+++ test/Dappacord.test.js
@@ -1,24 +1,44 @@
 const { expect } = require("chai");
 
+// WEI => ETH converter
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), "ether");
 };
 
 describe("Dappcord", function () {
-  describe("Deployment", function () {
-    it("Should set the name", async () => {
-      const Dappcord = await ethers.getContractFactory("Dappcord");
-      dappcord = await Dappcord.deploy("Dappcord", "DC");
+  let deployer, user;
+  let dappcord;
 
+  const NAME = "Dappcord";
+  const SYMBOL = "DC";
+
+  beforeEach(async () => {
+    // Setup accounts
+    [deployer, user] = await ethers.getSigners();
+
+    // Deploy the smart contract
+    const Dappcord = await ethers.getContractFactory("Dappcord");
+    dappcord = await Dappcord.deploy(NAME, SYMBOL);
+  });
+
+  describe("Deployment", function () {
+    it("Should set the Name", async () => {
       // Fetch name
       let result = await dappcord.name();
       // Check name
-      expect(result).to.equal("Dappcord");
+      expect(result).to.equal(NAME);
+    });
 
+    it("Should set the Symbol", async () => {
       // Fetch symbol
       result = await dappcord.symbol();
       // Check symbol
-      expect(result).to.equal("DC");
+      expect(result).to.equal(SYMBOL);
+    });
+
+    it("Should set the owner", async () => {
+      const result = await dappcord.owner();
+      expect(result).to.equal(deployer.address);
     });
   });
 });
